dc3c92333318629dfdccba854829a1f428172828
minhee231 <goominhee123@gmail.com>
2024-11-18 17:57:47 +0900

    :sparkles:feat: 기타 계산기 인풋 박스 추가

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,7 @@ export default {
   name: 'App',
 
   components: {
-    HelloWorld,
-    GuitarCalc
+    GuitarCalc,
   },
 
   data: () => ({
--- src/components/GuitarCalc.vue
+++ src/components/GuitarCalc.vue
@@ -1,6 +1,24 @@
 <template>
-    {{ test }}
     {{ latestCurrency }}
+    
+    
+
+    <v-container class="fill-height" fluid>
+    <v-row class="justify-center align-center ">
+    <v-card class="pa-4 container-custom portfolio-card rounded-xl" outlined>
+        <v-text-field 
+        label="¥가격 입력" 
+        v-model="inputJPYCost" 
+        @input="getKRWCost"
+    ></v-text-field>
+    
+    <v-col>
+        <span>{{ guitarCost.KRW }}원</span>
+        <span>{{ guitarCost.KRW }}</span> 
+    </v-col>
+  </v-card>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -14,27 +32,36 @@ export default {
     },
 
     data: () => ({
-        test: {}
+        inputJPYCost: "",
+        test: 0,
+        guitarCost: {},
     }),
 
     methods: {
-        getKRWCost(inputJPYCost) {
-            console.log(this.latestCurrency.currency.KRW);
-             this.test = Math.floor(inputJPYCost * this.latestCurrency.currency.KRW);
-            
-        }
-    },
+        getKRWCost() {
+            const sanitizedInput = String(this.inputJPYCost).replace(/[^0-9]/g, ""); 
+            const numericInput = parseFloat(sanitizedInput);
 
-    watch: {
-        //테스트임
-        latestCurrencyData: {
-            immediate: true,
-            handler(newData) {
-                if (newData.currency?.KRW) {
-                    this.getKRWCost(1);
-                }
+            if (!isNaN(numericInput)) {
+                console.log(this.latestCurrency.currency.KRW);
+                console.log(numericInput);
+                this.guitarCost.KRW = Math.floor(numericInput * this.latestCurrency.currency.KRW);
+            } else {
+                this.test = 0;
             }
         }
-    }
+    },
 }
 </script>
+
+<style scoped>
+
+.portfolio-card {
+    min-width: 330px;
+    text-align: center;
+}
+
+/*.container-custom {
+    background-color: #517FF5;
+} */
+</style>
