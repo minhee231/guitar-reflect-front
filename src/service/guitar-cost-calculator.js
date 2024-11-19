7c1dd1c9a10846e3c57c0a1ea10eb694d1e14b4e
minhee231 <goominhee123@gmail.com>
2024-11-19 14:07:01 +0900

    🔨refactor: 함수 간소화
    똑같은 작업을 하는 반복하는 이름만 다른 함수 있어서 하나로 줄임

--- src/service/guitar-cost-calculator.js
+++ src/service/guitar-cost-calculator.js
@@ -1,25 +1,6 @@
 export default {
-    jpyToKrw(jpy , krw) {
-        return Math.floor(jpy * krw);
-    },
-
-    jpyToUsd(jpy, usd) {
-        return Math.floor(jpy * usd);
-    },
-
-    krwToJpy(krw, jpy) {
-        return Math.floor(krw * jpy);
-    },
-
-    krwToUsd(krw, usd) {
-        return Math.floor(krw * usd);
-    },
-
-    usdToKrw(usd, krw) {
-        return Math.floor(usd * krw);
-    },
-
-    usdToKrw(usd, jpy) {
-        return Math.floor(usd * jpy);
-    },
+    convertCurrency(amount, exchangeRate) {
+        return amount * exchangeRate;
+    }
+    
 }
