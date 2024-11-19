365de218d7cac00d9df295158ddb145dab3d3171
minhee231 <goominhee123@gmail.com>
2024-11-19 11:46:45 +0900

    feat: 환율 계산 추가
    krw to jpy 등등 전부 추가함

--- src/service/guitar-cost-calculator.js
+++ src/service/guitar-cost-calculator.js
@@ -1,5 +1,25 @@
 export default {
     jpyToKrw(jpy , krw) {
         return Math.floor(jpy * krw);
-    }
+    },
+
+    jpyToUsd(jpy, usd) {
+        return Math.floor(jpy * usd);
+    },
+
+    krwToJpy(krw, jpy) {
+        return Math.floor(krw * jpy);
+    },
+
+    krwToUsd(krw, usd) {
+        return Math.floor(krw * usd);
+    },
+
+    usdToKrw(usd, krw) {
+        return Math.floor(usd * krw);
+    },
+
+    usdToKrw(usd, jpy) {
+        return Math.floor(usd * jpy);
+    },
 }
