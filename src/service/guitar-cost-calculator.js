abc922f970b6d6d97423df652c2bc9956058ccbf
minhee231 <goominhee123@gmail.com>
2024-11-19 14:26:34 +0900

    ✨feat: 부가가치세 계산 함수 추가

--- src/service/guitar-cost-calculator.js
+++ src/service/guitar-cost-calculator.js
@@ -1,6 +1,10 @@
 export default {
     convertCurrency(amount, exchangeRate) {
         return amount * exchangeRate;
-    }
+    },
+
+    calculateGuitarVAT(guitarCost) {
+        return guitarCost * 1.1;
+    },
     
 }
