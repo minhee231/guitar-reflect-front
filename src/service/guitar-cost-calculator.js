3d9a4cd3d4f58f519f6dcb1d41e2016a9c6e4248
minhee231 <goominhee123@gmail.com>
2024-11-19 14:37:03 +0900

    ✨feat: 관세 계산 함수 추가

--- src/service/guitar-cost-calculator.js
+++ src/service/guitar-cost-calculator.js
@@ -7,4 +7,17 @@ export default {
         return guitarCost * 1.1;
     },
     
+    calculateGuitarDuty(guitarCostUsd) {
+        let finalCost = guitarCostUsd;
+    
+        if (guitarCostUsd > 800) {
+            finalCost -= 800;
+            finalCost *= 1.08;
+        }
+    
+        return finalCost;
+    },
+    
+    
+    
 }
