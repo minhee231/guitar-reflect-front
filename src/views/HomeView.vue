beb53219b50beaa10b57da148673c9af3f0f373c
minhee231 <goominhee123@gmail.com>
2024-12-04 17:41:39 +0900

    :gem:style: 설명. 계산 컴포넌트 가로로 나란히 있게 수정

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,37 +1,42 @@
 <template>
-    <v-container>
-        <GuitarCalc :latestCurrency="latestCurrency"/>
-        <GuitarCalcDescription class="text-center GuitarCalcDescription "/>
-
+    <v-container fluid class="d-flex align-center" style="height: 80vh;"> <!-- 세로 중앙 정렬을 위해 전체 높이를 100vh로 설정 -->
+      <v-row justify="center"  class="w-100">
+        <v-col cols="3" class="text-center">
+          <GuitarCalcDescription class="GuitarCalcDescription" />
+        </v-col>
+        <v-col cols="6" class="text-center">
+          <GuitarCalc :latestCurrency="latestCurrency" />
+        </v-col>
+        <v-col cols="3" class="text-center">
+        </v-col>
+      </v-row>
     </v-container>
-    
-</template>
-
-<script>
-import GuitarCalc from '@/components/GuitarCalc.vue';
-import GuitarCalcDescription from '@/components/GuitarCalcDescription.vue';
-
-export default {
+  </template>
+  
+  <script>
+  import GuitarCalc from '@/components/GuitarCalc.vue';
+  import GuitarCalcDescription from '@/components/GuitarCalcDescription.vue';
+  
+  export default {
     name: "HomeView",
     components: {
-      GuitarCalc,  
+      GuitarCalc,
       GuitarCalcDescription,
     },
-
+  
     props: {
-        latestCurrency: {
-            type: Object,
-            required: true
-        },
+      latestCurrency: {
+        type: Object,
+        required: true
+      },
     },
-}
-
-</script>
-
-<style>
-.GuitarCalcDescription {
-    margin:50px auto 0; 
-    max-width: 20%;
+  }
+  </script>
+  
+  <style>
+  .GuitarCalcDescription {
+    max-width: 100%;
     display: block;
-}
-</style>
+  }
+  </style>
+  
