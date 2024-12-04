f9a8862c18ac6be238ae60a5d7b61a0eeb19ef88
minhee231 <goominhee123@gmail.com>
2024-12-04 16:35:04 +0900

    :gem:style: 헤더 버튼 위치 수정
    가운데로 이동함

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,37 +1,33 @@
 <template>
-    <v-app id="inspire">
-      <v-app-bar flat color="black">
-        <v-container class="mx-auto d-flex align-center justify-center" >
-          <v-avatar
-            class="me-4 "
-            color="grey-darken-4"
-            size="32"
-          > <v-img :src="headerIcon">
-            </v-img>
+  <v-app id="inspire">
+    <v-app-bar flat color="black">
+      <v-container class="mx-auto d-flex justify-center align-center">
+        <v-avatar
+          class="me-4"
+          color="grey-darken-4"
+          size="32"
+        >
+          <v-img :src="headerIcon"></v-img>
         </v-avatar>
-  
-          <v-btn
-            v-for="link in links"
-            :key="link"
-            :text="link"
-            variant="text"
-          ></v-btn>
-          <v-spacer></v-spacer>
-  
-        </v-container>
-      </v-app-bar>
-  
+        
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          :text="link"
+          variant="text"
+        ></v-btn>
+      </v-container>
+    </v-app-bar>
+  </v-app>
+</template>
 
-    </v-app>
-  </template>
-  
-  <script setup>
-  import headerIcon from '@/assets/headerIcon.png'
+<script setup>
+import headerIcon from '@/assets/headerIcon.png'
 
-    const links = [
-      'Dashboard',
-      'Messages',
-      'Profile',
-      'Updates',
-    ]
-  </script>
+const links = [
+  'Dashboard',
+  'Messages',
+  'Profile',
+  'Updates',
+]
+</script>
