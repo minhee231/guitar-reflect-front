8ab143140bda425e51da8a7b031159672f127a8f
minhee231 <goominhee123@gmail.com>
2024-12-05 17:23:25 +0900

    ✨feat: 관세 등 금액 관련 설명 컴포넌트 추가
    관세면제한도, 관세적용,관세감면,부가가치세,일본면세한도 추가했음

--- src/components/GuitarCalcDescription.vue
+++ src/components/GuitarCalcDescription.vue
@@ -1,32 +1,40 @@
 <template>
-    <v-card
-      v-for="(item, index) in cardTitles"
-      :key="index"
-      variant="outlined"
-      class="mb-4"
-    >
-      <v-card-title>{{ item }}</v-card-title>
-      <v-card-text>
+  <!-- <v-carousel hide-delimiters height="110" cycle :show-arrows="false"> -->
+  <v-carousel hide-delimiters height="200" cycle :show-arrows="false">
+    <v-carousel-item v-for="(item,index) in cardTitles" :key="index+'title'">
+      <v-card class="custom-card" variant="outlined">
+        <v-card-title>{{ item }}</v-card-title>
+        <v-card-text>
         {{ cardText[index] }}
       </v-card-text>
-    </v-card>
+      </v-card>
+    </v-carousel-item>
+  </v-carousel>
 </template>
-<!-- https://play.vuetifyjs.com/#eNp9Uk1r3DAQ/StCpwTWdmFbCmaz9IMc2kNb2t6iHBR71qusVhLS2N5l8X/PWP6IE0LAGPnNG703z3N34cEX2Vfn0qYGnvMNwtFpibAVhrFNk0jnWKFlCDeCP1RJ5eGclNIfwCQfBY+syGuVKW3LmuRoS9BEHgDBWdjbNpHe2zaM9EVDokiQunbWU49hyjANpsI9NeYHOPfgLDN2FtKXrJFeSYNUtzVqZaB8QZuJCSrUsL1cmGFdt8leoItrx8KzQwIWFqdBZ3TIhz4pIDpvskVw9BkKrxxGEpyc9chK2MlaI7sMN5USZc6urtnNll2NGBtnz9l6NSGDWs4+jEh3HQ9dLzmL8BXHUFizU1X6GKyhHxmvFLywR6c0+N8OlTVB8HwyILjU2rY/I4a+hlGAevZQHN7AH8OpxwT/4yGAb0DwuYbSV4BD+fbfLzjReS7SStSa2O8U/0Kwuu49DrRvtSnJ9oIX3f449lkqU/0PtycEE6aheqMxl8gXnLb5+zujP9tdp+sxT3ooR1qlQ3gdYojdd4LvEV3Is6woDXFo0VXjUwOYGXfMSBPV7vxlnX5OP2WlCjhBiZYPIY3X3A9C3crUWsfX/RPGuSJW -->
 <script>
 export default {
   name: "GuitarCalcDescrip",
   data: () => ({
     cardTitles: [
-      "관세",
+      "관세 면제 한도",
+      "관세 적용",
       "관세 감면",
       "부가가치세",
-      "택스프리/리펀",
+      "일본 면세 한도",
     ],
-    cardText: [
-      "관세 면제 한도는 직구: 150달러, 현지 구매: 800달러입니다.",
-      "내용2",
-      "내용3",
+    cardText: [ 
+      "관세 면제 한도는 직구의 경우 150달러, 현지 구매의 경우 800달러입니다.",
+      "관세는 면세 한도를 초과한 금액에 대해 8%가 부과됩니다.",
+      "자진신고 시, 관세의 30%를 감면받을 수 있으며, 최대 20만원까지 감면 혜택을 받을 수 있습니다.",
+      "부가가치세는 관세가 부과된 금액에 10%가 추가로 부과됩니다.",
+      "5,000엔 이상 구매 시 면세 혜택을 받을 수 있으며, 면세 한도는 최대 500,000엔까지 적용됩니다.",
     ]
   }),
 }
 </script>
+
+<style scoped>
+.custom-card {
+  min-height: 30%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
     <v-container fluid class="d-flex align-center" style="height: 80vh;"> <!-- 세로 중앙 정렬을 위해 전체 높이를 100vh로 설정 -->
       <v-row justify="center"  class="w-100">
-        <v-col cols="3" class="text-center">
+        <v-col cols="3" class="d-flex align-center justify-center text-center">
           <GuitarCalcDescription class="GuitarCalcDescription" />
         </v-col>
         <v-col cols="6" class="text-center">
@@ -37,6 +37,7 @@
   .GuitarCalcDescription {
     max-width: 100%;
     display: block;
+    min-width: 30%;
   }
   </style>
   
