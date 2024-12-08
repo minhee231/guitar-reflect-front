0c7d05712d49d9896aa3df88b21c79ae34fe4a39
minhee231 <minhee231>
2024-12-08 22:54:16 +0900

    :gem:style: 관세 설명 위치 변경 왼쪽 -> 계산기 밑

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,16 @@
 <template>
-    <v-container fluid class="d-flex align-center" style="height: 80vh;"> <!-- 세로 중앙 정렬을 위해 전체 높이를 100vh로 설정 -->
-      <v-row justify="center"  class="w-100">
-        <v-col cols="3" class="d-flex align-center justify-center text-center">
-          <GuitarCalcDescription class="GuitarCalcDescription" />
-        </v-col>
-        <v-col cols="6" class="text-center">
-          <GuitarCalc :latestCurrency="latestCurrency" />
-        </v-col>
-        <v-col cols="3" class="text-center">
-        </v-col>
-      </v-row>
-    </v-container>
+  <v-container fluid class="d-flex align-center" style="height: 80vh;">
+    <v-row>
+      <v-col cols="3">
+      </v-col>
+      <v-col cols="6">
+        <GuitarCalc :latestCurrency="latestCurrency" style="margin: 30px 0;"/>
+        <GuitarCalcDescription class="GuitarCalcDescription" />
+      </v-col>
+      <v-col cols="3">
+      </v-col>
+    </v-row>
+  </v-container>
   </template>
   
   <script>
