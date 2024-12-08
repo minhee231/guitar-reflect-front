9e61c9e3b420434daa931a863b14e8ebfcec0c83
minhee231 <minhee231>
2024-12-09 00:35:36 +0900

    :gem: 버튼 위치 수정
    홈 버튼 왼쪽에 위치, 기타 버튼들 오른쪽에 위치

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,21 +1,39 @@
 <template>
   <v-app id="inspire">
     <v-app-bar flat color="black">
-      <v-container class="mx-auto d-flex justify-center align-center">
-        <v-avatar
-          class="me-4"
-          color="grey-darken-4"
-          size="32"
-        >
-          <v-img :src="headerIcon"></v-img>
-        </v-avatar>
-        
-        <v-btn
-          v-for="link in links"
-          :key="link"
-          :text="link"
-          variant="text"
-        ></v-btn>
+      <v-container>
+        <v-row>
+          <!-- 왼쪽 공백 -->
+          <v-col cols="3" />
+          
+          <!-- 가운데 아이콘과 버튼 -->
+          <v-col cols="6">
+            <div class="d-flex justify-space-between align-center">
+              <v-btn>
+                <!-- <v-avatar color="grey-darken-4" size="40"> -->
+                  <v-img :width="40" :src="guitarIcon"></v-img>
+                <!-- </v-avatar> -->
+                guitar-Reflect
+              </v-btn>
+
+              <!-- 버튼 -->
+              <div>
+                <v-btn
+                  v-for="link in links"
+                  :key="link"
+                  class="mx-2"
+                  variant="text"
+                  text
+                >
+                  {{ link }}
+                </v-btn>
+              </div>
+            </div>
+          </v-col>
+
+          <!-- 오른쪽 공백 -->
+          <v-col cols="3" />
+        </v-row>
       </v-container>
     </v-app-bar>
   </v-app>
@@ -23,6 +41,7 @@
 
 <script setup>
 import headerIcon from '@/assets/headerIcon.png'
+import guitarIcon from '@/assets/guitar-icon-3rd.png'
 
 const links = [
   'Dashboard',
