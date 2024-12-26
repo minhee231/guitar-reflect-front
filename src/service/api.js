df3df67ec3872854023df2d16aa59023b562b17c
minhee231 <goominhee123@gmail.com>
2024-12-26 11:27:13 +0900

    chore: api 개발 주소로 변경경

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: 'https://api.guitar.x-coll.com',
+    baseURL: 'http://localhost:8081',
     headers: {
         'Content-Type': 'application/json',
     }
