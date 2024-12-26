046d04bba04efacb9ee344dca9604a77c22de721
minhee231 <goominhee123@gmail.com>
2024-12-26 11:24:24 +0900

    chore: api 도메인 변경 로컬 -> api 서버 주소로 변경

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8081',
+    baseURL: 'https://api.guitar.x-coll.com',
     headers: {
         'Content-Type': 'application/json',
     }
